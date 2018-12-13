5c59c5a03a5a45b08e833b5d3ab9568f7befc25c
oumarniass <oumar.s@codeur.online>
2018-12-13 17:21:08 +0100

    api projet start

---
+++ src/Entity/Test.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\TestRepository")
+ */
+class Test
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_naissance_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateNaissanceAt(): ?\DateTimeInterface
+    {
+        return $this->date_naissance_at;
+    }
+
+    public function setDateNaissanceAt(\DateTimeInterface $date_naissance_at): self
+    {
+        $this->date_naissance_at = $date_naissance_at;
+
+        return $this;
+    }
+}
